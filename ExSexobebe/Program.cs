b4c0d9007153c811436c11f56c7d85e2c2ab1ae2
00111259 <00111259@CIDLAB103M4>
2023-03-23 21:31:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExSexobebe/Program.cs
@@ -0,0 +1,21 @@
+﻿//Fazer um algoritmo em para ler o código de um determinado produto e mostrar a sua classificação.
+
+using System;
+
+string codigo;
+char sexoBebe;
+
+Console.WriteLine($"{Environment.NewLine} ========= Sexo do bebê ========= ");
+
+    Console.WriteLine($"{Environment.NewLine}Informe o código do produto: ");
+    sexoBebe = Convert.ToChar(Console.ReadLine().ToUpper);
+
+switch (sexoBebe)
+{
+
+}
+
+
+Console.WriteLine($"{Environment.NewLine}Programa finalizado. ");
+
+
